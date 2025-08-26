5dbd4e9ee0db30ab99ac5b93d6e4f8cfdaa1b587
Dominic Clifton <me@dominicclifton.name>
2025-08-26 13:48:09 +0200

    Fix table automatically resizing horizontally and vertically when unconstrained.

--- src/lib.rs
+++ src/lib.rs
@@ -116,7 +116,7 @@ impl<DataSource> DeferredTable<DataSource> {
 
         // if there is content above the table, we use this min rect so we to define an area starting at the right place.
         let outer_min_rect = Rect::from_min_size(outer_next_widget_position, state.min_size.clone());
-        let outer_max_rect = Rect::from_min_size(outer_next_widget_position, parent_clip_rect.size());
+        let outer_max_rect = Rect::from_min_size(outer_next_widget_position, parent_max_rect.size());
         trace!("outer_min_rect: {:?}", outer_min_rect);
         trace!("outer_max_rect: {:?}", outer_max_rect);
 
