a1a6b74f780c7556e3aebb16c68e3ed664734099
Dominic Clifton <me@dominicclifton.name>
2025-09-30 14:17:30 +0200

    First cut of editable table support.

--- src/lib.rs
+++ src/lib.rs
@@ -1697,3 +1697,132 @@ mod reordering_tests {
         assert_eq!(ordering, Some(expected));
     }
 }
+
+///
+/// Editable table support
+///
+
+#[derive(Debug, Clone)]
+pub enum CellEditState<E, T> {
+    /// the pivot point for selections, etc.
+    Pivot(CellIndex),
+    /// when editing a cell, we need state for the cell and a copy of the original row to be able to track changes
+    Editing(CellIndex, E, T),
+}
+
+pub trait ApplyChange<T, E> {
+    fn apply_change(&mut self, value: T) -> Result<(), E>;
+}
+
+/// Implement this to enable data source editing support.
+///
+/// Usually implemented on the table renderer, not the data source.
+///
+/// The implementation needs to own a `CellEditState`.
+/// typically the renderer just has a member like this: `edit_state: Option<CellEditState<MyItemState, MyRow>>`
+pub trait EditableTableRenderer<DataSource> {
+    /// Usually a type containing the data for a single row.
+    type Value;
+    /// Usually an enum, with variants for each type of cell that can be edited.
+    /// e.g. `Number(f32)`, `Text(String)`...
+    type ItemState;
+
+    /// Called when the cell needs to be edited.
+    ///
+    /// Return None to prevent editing or a tuple containing the ItemState and the original value.
+    fn build_item_state(
+        &self,
+        cell_index: CellIndex,
+        source: &mut DataSource,
+    ) -> Option<(Self::ItemState, Self::Value)>;
+
+    /// Called when the cell is no-longer being edited.
+    ///
+    /// Implementations usually modify the data source directly, or build and send a command that will change
+    /// eventually update the datasource, e.g. via a message queue and/or in a background thread.
+    fn on_edit_complete(
+        &mut self,
+        index: CellIndex,
+        state: Self::ItemState,
+        original_item: Self::Value,
+        source: &mut DataSource,
+    );
+
+    // the following three methods are used to update the cell edit state instance, example implementations
+    // are provided when `self` owns an
+
+    /// ```ignore
+    /// fn set_edit_state(&mut self, edit_state: CellEditState<Self::ItemState, Self::Value>) {
+    ///     self.edit_state.replace(edit_state);
+    /// }
+    /// ```
+    fn set_edit_state(&mut self, edit_state: CellEditState<Self::ItemState, Self::Value>);
+
+    /// ```ignore
+    /// fn edit_state(&self) -> Option<&CellEditState<Self::ItemState, Self::Value>> {
+    ///     self.edit_state.as_ref()
+    /// }
+    /// ```
+    fn edit_state(&self) -> Option<&CellEditState<Self::ItemState, Self::Value>>;
+
+    /// ```ignore
+    /// fn take_edit_state(&mut self) -> CellEditState<Self::ItemState, Self::Value> {
+    ///     self.edit_state.take().unwrap()
+    /// }
+    /// ```
+    fn take_edit_state(&mut self) -> CellEditState<Self::ItemState, Self::Value>;
+}
+
+/// call this function from a cell action handler
+pub fn handle_editable_cell_click<
+    E,
+    S,
+    R: EditableTableRenderer<S, Value = T, ItemState = E>,
+    T: Clone,
+>(
+    source: &mut S,
+    renderer: &mut R,
+    cell_index: CellIndex,
+) {
+    match renderer.edit_state() {
+        None => {
+            // change selection
+            renderer.set_edit_state(CellEditState::Pivot(cell_index));
+        }
+        Some(CellEditState::Pivot(pivot_cell_index)) if *pivot_cell_index == cell_index => {
+            trace!("clicked in selected cell");
+
+            // change mode to edit
+            let edit_state = renderer.build_item_state(cell_index, source);
+            if let Some((edit, original_item)) = edit_state {
+                renderer.set_edit_state(CellEditState::Editing(cell_index, edit, original_item));
+            }
+        }
+        Some(CellEditState::Pivot(_)) => {
+            trace!("clicked in different cell");
+
+            // change selection
+            renderer.set_edit_state(CellEditState::Pivot(cell_index));
+        }
+        Some(CellEditState::Editing(editing_cell_index, _cell_edit_state, _original_item))
+            if *editing_cell_index == cell_index =>
+        {
+            trace!("clicked in cell while editing");
+
+            // nothing to do
+        }
+        Some(CellEditState::Editing(_editing_cell_index, _cell_edit_state, _original_item)) => {
+            trace!("clicked in a different cell while editing");
+
+            // apply edited value
+            let CellEditState::Editing(index, state, original_item) = renderer.take_edit_state()
+            else {
+                unreachable!();
+            };
+            renderer.on_edit_complete(index, state, original_item, source);
+
+            // change selection
+            renderer.set_edit_state(CellEditState::Pivot(cell_index));
+        }
+    }
+}
